fe63d1c1001eb93eba1f7e6e6e7d672d54c93726
Giovanna Bertoldo <giovannabertoldo81@gmail.com>
2026-05-25 13:16:50 -0300

    Rename JogoDaVelhaTest class to JogoDaVelhaTeste

--- src/teste/java/JogoDaVelhaTeste.java
+++ src/teste/java/JogoDaVelhaTeste.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
-public class JogoDaVelhaTest {
+public class JogoDaVelhaTeste {
 
     @Test
     void verificarJogador() {
